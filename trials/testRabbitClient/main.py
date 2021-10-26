ae6df0d64596d19f841574943734d02c077aae93
Márta Boldizsár <marta.boldi13@gmail.com>
2021-10-27 00:30:43 +0200

    basic test client for sending rabbit messages

--- trials/testRabbitClient/main.py
+++ trials/testRabbitClient/main.py
@@ -5,13 +5,12 @@ import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.57.131'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='set-mode')
 
 channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello Dori!')
+                      routing_key='move-robot',
+                      body='(0.2,0,0)')
 
-print(" [x] Sent 'Hello World!'")
 
 
 connection.close()
