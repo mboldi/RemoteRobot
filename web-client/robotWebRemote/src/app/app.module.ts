a135ab20c013ed1c6f081c81ff867a91ae657072
Márta Boldizsár <marta.boldi13@gmail.com>
2022-03-09 19:17:08 +0100

    settings page

--- web-client/robotWebRemote/src/app/app.module.ts
+++ web-client/robotWebRemote/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BasicControlComponent } from './pages/basic-control/basic-control.component';
-import { BasicControlPanelComponent } from './components/basic-control-panel/basic-control-panel.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BasicControlComponent} from './pages/basic-control/basic-control.component';
+import {BasicControlPanelComponent} from './components/basic-control-panel/basic-control-panel.component';
+import {SettingsComponent} from './pages/settings/settings.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from "@angular/material/button";
-import { HomeComponent } from './pages/home/home.component';
+import {HomeComponent} from './pages/home/home.component';
 import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -30,9 +33,13 @@ import {MatInputModule} from "@angular/material/input";
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- web-client/robotWebRemote/src/app/pages/settings/settings.component.ts
+++ web-client/robotWebRemote/src/app/pages/settings/settings.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {SettingsService} from "../../services/settings.service";
 
 @Component({
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  settingsForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private settingsService: SettingsService) {
+    this.settingsForm = this.fb.group({
+      serverIp: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  save() {
+    this.settingsService.setRabbitUrl(this.settingsForm.value.serverIp)
+  }
 }
